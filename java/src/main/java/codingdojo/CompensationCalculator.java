bf465a7e35ba27c08bd3c98b62b808d0fc5eb2a0
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-05-13 22:44:47 +0000

    R - effectively inline
    Co-Authored-By: brianberzins <2465816+brianberzins@users.noreply.github.com>

--- java/src/main/java/codingdojo/CompensationCalculator.java
+++ java/src/main/java/codingdojo/CompensationCalculator.java
@@ -9,7 +9,6 @@ public class CompensationCalculator {
     public static final int THRESHOLD_OVERTIME_HOURS_RATE_2 = 6;
 
     public static Overtime calculateOvertime(BigDecimal hoursOvertimeTotal, Assignment assignment, Briefing briefing) {
-        BigDecimal hoursOvertimeRate2 = BigDecimal.ZERO;
 
         boolean isWatcodeUnion = briefing.watcode() && assignment.isUnionized();
         boolean isWatcodeNonUnionForeign = briefing.watcode() && !assignment.isUnionized() && briefing.foreign();
@@ -20,19 +19,20 @@ public class CompensationCalculator {
                 || isWatcodeUnion
                 || (briefing.foreign() && !assignment.isUnionized());
         if (isApplesauce) {
-            return new Overtime(hoursOvertimeTotal, hoursOvertimeRate2);
+            return new Overtime(hoursOvertimeTotal, BigDecimal.ZERO);
         } else if (hoursOvertimeTotal.compareTo(BigDecimal.ZERO) < 1) {
-            return new Overtime(BigDecimal.ZERO, hoursOvertimeRate2);
+            return new Overtime(BigDecimal.ZERO, BigDecimal.ZERO);
         } else if (hoursOvertimeTotal.compareTo(MAX_OVERTIME_HOURS_RATE_1) < 1) {
-            return new Overtime(hoursOvertimeTotal, hoursOvertimeRate2);
+            return new Overtime(hoursOvertimeTotal, BigDecimal.ZERO);
         } else if (assignment.isUnionized()) {
+            BigDecimal hoursOvertimeRate3 = BigDecimal.ZERO;
             BigDecimal threshold = calculateThreshold(assignment, THRESHOLD_OVERTIME_HOURS_RATE_2);
-            hoursOvertimeRate2 = hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1);
-            hoursOvertimeRate2 = hoursOvertimeRate2.min(threshold);
-            return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeRate2);
+            hoursOvertimeRate3 = hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1);
+            hoursOvertimeRate3 = hoursOvertimeRate3.min(threshold);
+            return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeRate3);
         } else {
-            hoursOvertimeRate2 = hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1);
-            return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeRate2);
+            var hoursOvertimeRate3 = hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1);
+            return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeRate3);
         }
     }
 
