53826193e918f8893ca2d59e98ed9ab5e3b8c587
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-05-13 22:24:46 +0000

    t - update approvalstests, wrote a combination test
    Co-Authored-By: brianberzins <2465816+brianberzins@users.noreply.github.com>

--- java/src/test/java/codingdojo/CompensationCalculatorTest.java
+++ java/src/test/java/codingdojo/CompensationCalculatorTest.java
@@ -1,13 +1,34 @@
 package codingdojo;
 
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
+import org.lambda.query.Queryable;
+
+import java.math.BigDecimal;
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CompensationCalculatorTest {
     @Test
     void fixme() {
-        assertEquals(true, false);
+
+
+        var overtimes = Queryable.as(0.0, 0.5, 1.0, 5.0, 20.0).select(BigDecimal::new).asArray();
+        Boolean[] booleans = new Boolean[]{true, false};
+        Duration[] durations = Queryable.as(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10).select(Duration::ofHours).asArray();
+        CombinationApprovals.verifyAllCombinations(
+                this::callOvertime,
+                 overtimes, booleans, durations, booleans, booleans, booleans, booleans);
+
+    }
+
+    private Object callOvertime(BigDecimal bigDecimal, boolean isUnionized, Duration duration, boolean watcode, boolean z3, boolean foreign, boolean hbmo) {
+        return CompensationCalculator.calculateOvertime(
+                bigDecimal,
+                new Assignment(isUnionized, duration),
+                new Briefing(watcode, z3, foreign, hbmo)
+        );
     }
 
 }
