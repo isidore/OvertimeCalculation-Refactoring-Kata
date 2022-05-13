45e9fa9904182f77c0b20b0d60055e40a930426b
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-05-13 23:25:05 +0000

    R - remove unused condition
    Co-Authored-By: brianberzins <2465816+brianberzins@users.noreply.github.com>

--- java/src/main/java/codingdojo/UnderMaxOvertime.java
+++ java/src/main/java/codingdojo/UnderMaxOvertime.java
@@ -5,12 +5,11 @@ import codingdojo.overtimecalculator.OvertimeCalculator;
 import java.math.BigDecimal;
 
 import static codingdojo.CompensationCalculator.MAX_OVERTIME_HOURS_RATE_1;
-import static codingdojo.CompensationCalculator.isApplesauce;
 
 public class UnderMaxOvertime implements OvertimeCalculator {
     @Override
     public boolean isValidFor(BigDecimal hoursOvertimeTotal, Assignment assignment, Briefing briefing) {
-        return !isApplesauce(assignment, briefing) && hoursOvertimeTotal.compareTo(MAX_OVERTIME_HOURS_RATE_1) < 1;
+        return hoursOvertimeTotal.compareTo(MAX_OVERTIME_HOURS_RATE_1) < 1;
     }
 
     @Override
