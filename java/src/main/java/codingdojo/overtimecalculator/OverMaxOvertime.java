0a7d56c8d05e1a57070496597aec27f2e344a4be
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-05-14 16:19:09 +0000

    R pushing variables into Query
    Co-Authored-By: Lars Eckart <4414802+larseckart@users.noreply.github.com>
    Co-Authored-By: brianberzins <2465816+brianberzins@users.noreply.github.com>

--- java/src/main/java/codingdojo/overtimecalculator/OverMaxOvertime.java
+++ java/src/main/java/codingdojo/overtimecalculator/OverMaxOvertime.java
@@ -3,6 +3,7 @@ package codingdojo.overtimecalculator;
 import codingdojo.Assignment;
 import codingdojo.Briefing;
 import codingdojo.Overtime;
+import org.lambda.query.Queryable;
 
 import java.math.BigDecimal;
 
@@ -18,8 +19,10 @@ public class OverMaxOvertime implements OvertimeCalculator {
                 || briefing.z3()
                 || unionized;
 
-        var a2 = !(briefing.hbmo() && unionized);
-        var a4 = !(briefing.watcode() && unionized);
+        var a2 = !briefing.hbmo() || !unionized;
+        var a4 = !briefing.watcode() || !unionized;
+
+        var b1 = (!briefing.hbmo() || !unionized) && (!briefing.watcode() || !unionized);
 
         var a3 = !(briefing.watcode()
                 && !unionized
@@ -27,11 +30,9 @@ public class OverMaxOvertime implements OvertimeCalculator {
         var a5 = !(briefing.foreign()
                 && !unionized);
 
-        var isApplesauce = a1
-                && a2
-                && a3
-                && a4
-                && a5;
+        var r = Queryable.as(a1, a2, a3, a4, a5);
+
+        var isApplesauce = r.all(b -> b);
 
         var exceedsMaxOvertime = 1 <= hoursOvertimeTotal.compareTo(MAX_OVERTIME_HOURS_RATE_1);
         return exceedsMaxOvertime && isApplesauce;
