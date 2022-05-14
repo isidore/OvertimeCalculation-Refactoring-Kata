e7b4c8b60c8cd52dff90c3da86a8f123af1aacc0
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-05-14 17:31:07 +0000

    t - adding test cases
    Co-Authored-By: Lars Eckart <4414802+larseckart@users.noreply.github.com>
    Co-Authored-By: brianberzins <2465816+brianberzins@users.noreply.github.com>

--- java/src/test/java/codingdojo/CompensationCalculatorTest.java
+++ java/src/test/java/codingdojo/CompensationCalculatorTest.java
@@ -7,17 +7,15 @@ import org.lambda.query.Queryable;
 import java.math.BigDecimal;
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class CompensationCalculatorTest {
     @Test
     void fixme() {
-        var overtimes = Queryable.as(0.0, 0.5, 1.0, 5.0, 20.0).select(BigDecimal::new).asArray();
+        var overtimes = Queryable.as(0.0, 0.5, 1.0, 5.0, 13.0, 15.0, 17.0, 20.0).select(BigDecimal::new).asArray();
         Boolean[] booleans = new Boolean[]{true, false};
         Duration[] durations = Queryable.as(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10).select(Duration::ofHours).asArray();
         CombinationApprovals.verifyAllCombinations(
                 this::callOvertime,
-                 overtimes, booleans, durations, booleans, booleans, booleans, booleans);
+                overtimes, booleans, durations, booleans, booleans, booleans, booleans);
 
     }
 
@@ -28,8 +26,6 @@ public class CompensationCalculatorTest {
                 new Briefing(watcode, z3, foreign, hbmo)
         );
     }
-
-
 
 
 }
