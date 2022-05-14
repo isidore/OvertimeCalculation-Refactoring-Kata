50deae19ac92b16146cbe4204f129221c647d5f9
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-05-14 17:09:59 +0000

    r remove temp variables
    Co-Authored-By: Lars Eckart <4414802+larseckart@users.noreply.github.com>
    Co-Authored-By: brianberzins <2465816+brianberzins@users.noreply.github.com>

--- java/src/main/java/codingdojo/overtimecalculator/DoubleOvertime.java
+++ java/src/main/java/codingdojo/overtimecalculator/DoubleOvertime.java
@@ -12,18 +12,19 @@ public class DoubleOvertime implements OvertimeCalculator {
 
     @Override
     public boolean isValidFor(BigDecimal hoursOvertimeTotal, Assignment assignment, Briefing briefing) {
+        return exceedsStandardOvertime(hoursOvertimeTotal) && isEligableForDoubleOvertime(assignment, briefing);
+    }
 
-        var unionized = assignment.isUnionized();
+    private boolean exceedsStandardOvertime(BigDecimal hoursOvertimeTotal) {
+        return isALessThanB(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeTotal);
+    }
 
-        var standardZ3 = briefing.z3() && (!unionized && !briefing.foreign());
-        var standardWatcode = briefing.watcode() && (!unionized && !briefing.z3() && !briefing.foreign());
-        var standardUnionized = unionized && (!briefing.watcode() && !briefing.hbmo());
-
+    private boolean isEligableForDoubleOvertime(Assignment assignment, Briefing briefing) {
+        var standardZ3 = briefing.z3() && !(assignment.isUnionized() || briefing.foreign());
+        var standardWatcode = briefing.watcode() && !(assignment.isUnionized() || briefing.z3() || briefing.foreign());
+        var standardUnionized = assignment.isUnionized() && !(briefing.watcode() || briefing.hbmo());
         var isEligableForDoubleOvertime = (standardZ3 || standardWatcode || standardUnionized);
-
-
-        var exceedsMaxOvertime = isALessThanB(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeTotal);
-        return exceedsMaxOvertime && isEligableForDoubleOvertime;
+        return isEligableForDoubleOvertime;
     }
 
     public static boolean isALessThanB(BigDecimal a, BigDecimal b) {
