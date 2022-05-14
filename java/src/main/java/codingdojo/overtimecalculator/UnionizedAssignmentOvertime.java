467d340c37b86c76d6ac0783dd510cae12d6273a
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-05-14 17:46:22 +0000

    R - rename/cleanup max double overtime
    Co-Authored-By: Lars Eckart <4414802+larseckart@users.noreply.github.com>
    Co-Authored-By: brianberzins <2465816+brianberzins@users.noreply.github.com>

--- java/src/main/java/codingdojo/overtimecalculator/UnionizedAssignmentOvertime.java
+++ java/src/main/java/codingdojo/overtimecalculator/UnionizedAssignmentOvertime.java
@@ -12,15 +12,7 @@ import static codingdojo.overtimecalculator.DoubleOvertime.WHEN_DOUBLE_OVERTIME_
 
 public class UnionizedAssignmentOvertime implements OvertimeCalculator {
     public static final int THRESHOLD_OVERTIME_HOURS_RATE_2 = 6;
-    private static final int THRESHOLD_OVERTIME_HOURS_RATE_2_FOREIGN = THRESHOLD_OVERTIME_HOURS_RATE_2;
-
-    public static BigDecimal calculateThreshold(Assignment assignment, long threshold) {
-        Duration remainder = assignment.duration().minusHours(threshold);
-        if (remainder.isNegative()) {
-            return BigDecimal.valueOf(assignment.duration().toSeconds() / 3600);
-        }
-        return BigDecimal.valueOf(threshold);
-    }
+    private static final int THRESHOLD_OVERTIME_HOURS_RATE_2_FOREIGN = 4;
 
     // The threshold for overtime hours at rate 2 should be changed from 6 to 4
     // if the assignment is unionized and the briefing is foreign.
@@ -34,10 +26,18 @@ public class UnionizedAssignmentOvertime implements OvertimeCalculator {
 
     @Override
     public Overtime calculateOvertime(BigDecimal hoursOvertimeTotal, Assignment assignment, Briefing briefing) {
+        var doubleOvertime = hoursOvertimeTotal.subtract(DoubleOvertime.WHEN_DOUBLE_OVERTIME_STARTS);
+        doubleOvertime = doubleOvertime.min(calculateMaxDoubleOvertimeAllowed(assignment, briefing));
+        return new Overtime(DoubleOvertime.WHEN_DOUBLE_OVERTIME_STARTS, doubleOvertime);
+    }
+
+    public static BigDecimal calculateMaxDoubleOvertimeAllowed(Assignment assignment, Briefing briefing) {
         var thresholdOvertimeHoursRate2 = briefing.foreign() ? THRESHOLD_OVERTIME_HOURS_RATE_2_FOREIGN : THRESHOLD_OVERTIME_HOURS_RATE_2;
-        BigDecimal threshold = calculateThreshold(assignment, thresholdOvertimeHoursRate2);
-        var hoursOvertimeRate2 = hoursOvertimeTotal.subtract(DoubleOvertime.WHEN_DOUBLE_OVERTIME_STARTS);
-        hoursOvertimeRate2 = hoursOvertimeRate2.min(threshold);
-        return new Overtime(DoubleOvertime.WHEN_DOUBLE_OVERTIME_STARTS, hoursOvertimeRate2);
+        Duration remainder = assignment.duration().minusHours(thresholdOvertimeHoursRate2);
+        if (remainder.isNegative()) {
+            return BigDecimal.valueOf(assignment.duration().toSeconds() / 3600);
+        } else {
+            return BigDecimal.valueOf(thresholdOvertimeHoursRate2);
+        }
     }
 }
